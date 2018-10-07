3d9e253ff305e6899feb0ad0c56004a431994f8a
Em Ji Madhu <emjimadhu@gmail.com>
2018-10-07 13:33:33 +0530

    Initial commit with Vuido Boilerplate

---
+++ README.md
@@ -0,0 +1,18 @@
+# jsonify
+
+> A simple JSON pretifier
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# build
+npm run build
+
+# run the bundled script
+npm start
+```
+
+For detailed explanation on how things work, consult the [Vuido documentation](https://vuido.mimec.org/).
---
+++ src/MainWindow.vue
@@ -0,0 +1,17 @@
+<template>
+  <Window title="passwordify" width="400" height="100" margined v-on:close="exit">
+    <Box>
+      <Text>Welcome to your Vuido application!</Text>
+    </Box>
+  </Window>
+</template>
+
+<script>
+export default {
+  methods: {
+    exit() {
+      this.$exit();
+    }
+  }
+}
+</script>
